6336ae3354adb67c6627f3195f086f769140c3fc
Adam <adam.isenberg4920@gmail.com>
2021-05-01 18:03:10 +0200

    added input prompts

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from Set import set, createFromFile
 
+print("Please enter your data in 'data.csv'. [Enter] to continue...")
+input()
 
 # retrieve data
 identifiers, xList, yData = createFromFile("data.csv")
+
+
+print("Enter the starting point of the data to train: (e.g: 1)")
+tStart = int(input())
+
+print("Enter the end point of the data to train: (e.g: 10)")
+tEnd = int(input())
