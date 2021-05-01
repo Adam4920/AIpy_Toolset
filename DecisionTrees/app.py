0734c7b4a0b805e81ad5de16ba22dffe90c30667
Adam <adam.isenberg4920@gmail.com>
2021-05-01 18:56:59 +0200

    added and instanciated instructions class

--- DecisionTrees/app.py
+++ DecisionTrees/app.py
@@ -1,4 +1,5 @@
 from Set import set, createFromFile
+from Instructions import instructions
 
 print("Please enter your data in 'data.csv'. [Enter] to continue...")
 input()
@@ -11,6 +12,8 @@ tStart = int(input())
 
 print("Enter the end point of the data to train: (e.g: 10)")
 tEnd = int(input())
+
+instr = instructions([tStart, tEnd])
 
 rawI = identifiers.data[tStart-1:tEnd]
 rawXList = xList[tStart-1:tEnd]
---
+++ Instructions.py
@@ -0,0 +1,8 @@
+class instructions:
+    def __init__(self, range):
+        self.output = []
+        self.range = range
+
+    def print():
+        for i in range(0, len(self.output)):
+            print(self.output[i])
