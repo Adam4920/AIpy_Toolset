d6f11d2b4fa02a1fc966724b2c97861612d72d8c
Adam <adam.isenberg4920@gmail.com>
2021-05-01 18:39:37 +0200

    created training set from input range

--- DecisionTrees/app.py
+++ DecisionTrees/app.py
@@ -6,9 +6,12 @@ input()
 # retrieve data
 identifiers, xList, yData = createFromFile("data.csv")
 
-
 print("Enter the starting point of the data to train: (e.g: 1)")
 tStart = int(input())
 
 print("Enter the end point of the data to train: (e.g: 10)")
 tEnd = int(input())
+
+rawI = identifiers.data[tStart-1:tEnd]
+rawXList = xList[tStart-1:tEnd]
+rawY = yData.data[tStart-1:tEnd]
